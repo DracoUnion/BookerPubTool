71c659a3ccbe40ee6c46a67e7185444ba4464c6e
wizardforcel <562826179@qq.com>
2026-03-25 16:49:21 +0800

    2026-03-25 16:49:21

--- BookerPubTool/__init__.py
+++ BookerPubTool/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""pypi-book-publisher
-https://github.com/apachecn/pypi-book-publisher"""
+"""BookerPubTool
+https://github.com/DracoUnion/BookerPubTool"""
 
 __author__ = "wizardforcel"
 __email__ = "wizard.z@qq.com"
 __license__ = "SATA"
-__version__ = "2023.7.9.0"
+__version__ = "2026.3.25.0"
